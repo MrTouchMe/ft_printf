3998832f8239b4d5a1ccb366ba3c02316891d787
dgiurgev <135585161+MrTouchMe@users.noreply.github.com>
2023-12-14 18:25:35 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,22 @@
 /*   By: dgiurgev <dgiurgev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:22:56 by dgiurgev          #+#    #+#             */
-/*   Updated: 2023/12/13 18:20:40 by dgiurgev         ###   ########.fr       */
+/*   Updated: 2023/12/14 18:20:27 by dgiurgev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_printf_helper(const char **format, int *len)
+{
+	if ((**format == ' ' || **format == '-'))
+	{
+		if (**format == ' ')
+			ft_putchar(**format, len);
+		while ((**format == ' ' || **format == '-'))
+			(*format)++;
+	}
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -21,9 +32,9 @@ int	ft_printf(const char *format, ...)
 	va_start(args, format);
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%' && format++)
 		{
-			format++;
+			ft_printf_helper(&format, &len);
 			if (*format == '%')
 				ft_putchar('%', &len);
 			else
